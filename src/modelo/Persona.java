bda69f5bca2a77767a008a6225dbcea25e218f19
JuanHdez <juanh7522@gmail.com>
2021-12-08 18:27:15 -0600

    restaurar

--- src/modelo/Persona.java
+++ src/modelo/Persona.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package modelo;
 
-/**
- *
- * @author Juan M. Hdez
- */
 public class Persona {
-  public String nombre;
-  public String apellido;
+  
   
 }
--- src/proyectorestaurante/ProyectoRestaurante.java
+++ src/proyectorestaurante/ProyectoRestaurante.java
@@ -1,32 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package proyectorestaurante;
-//Hola 
-//DDDDDDDDDDDDDDD
-/**
- *
- * @author Juan M. Hdez
- */
+
 public class ProyectoRestaurante {
 
   
   public static void main(String[] args) {
     // TODO code application logic here
     System.out.println("Hola mundo");
-    System.out.println("Adios mundo");
   }
-  //Adios
-  
-  
-  
-  
-  public void holaMundo(){
-    System.out.println("Hola mundo");
-
-    System.out.println("Adios");
-}
   
 }
